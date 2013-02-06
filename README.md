c90069fa9235bce92776f8d298f823060f3fba3d
scottmotte <scott@scottmotte.com>
2013-02-05 20:06:07 -0800

    Setting keys

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 Node.js Bindings for Signature.io's API.
 
 [![Build Status](https://travis-ci.org/scottmotte/signatureio-node.png)](https://travis-ci.org/scottmotte/signatureio-node)
+
+## Installation
+
+    $ npm install signatureio
+
+## Usage overview
+
+    var secret_api_key  = 'abc';  // Your Secret Signature.io API Key
+    var signatureio     = require('signatureio')(secret_api_key);
+
+    signatureio.documents.all(
+       { email: 'foobar@example.org' },
+       function(err, customer) {
+          if (err) {
+             console.log(err.message);
+             return;
+          }
+          console.log("customer id", customer.id);
+       }
+     );
--- test/main.js
+++ test/main.js
@@ -1,11 +1,23 @@
-var should    = require('should');
+var should      = require('should');
+var assert      = require('assert');
 var signatureio = require('../lib/main');
 
 describe('signatureio', function() {
   describe('with no arguments', function() {
-    it('returns an empty array', function() {
+    it('sets the secret and public keys to null', function() {
       var result = signatureio();
-      result.should.eql([]);
+
+      assert.equal(result.secret_api_key, null);
+      assert.equal(result.public_api_key, null);
+    });
+  });
+
+  describe('with arguments', function() {
+    it('sets the secret and public keys to their values', function() {
+      var result = signatureio("sk_1234", "pk_5678");
+
+      assert.equal(result.secret_api_key, "sk_1234");
+      assert.equal(result.public_api_key, "pk_5678");
     });
   });
 });
